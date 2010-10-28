f49a55eff0f53e40e2ee9fb77fd399f227fa43ef
Stephen Touset <stephen@touset.org>
2010-10-28 18:47:02 -0400

    Gemspec provided

--- lib/semantic_form_for.rb
+++ lib/semantic_form_for.rb
@@ -1,4 +1,6 @@
 module SemanticFormFor
+  require 'semantic_form_for/version'
+  
   autoload :FormBuilder, 'semantic_form_for/form_builder'
   autoload :FormHelpers, 'semantic_form_for/form_helpers'
   autoload :Railtie,     'semantic_form_for/railtie'
---
+++ lib/semantic_form_for/version.rb
@@ -0,0 +1,3 @@
+module SemanticFormFor
+  VERSION = '0.0.1'
+end
