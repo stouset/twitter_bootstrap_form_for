ba9f75769fe079cde8bf3c02ab5dbda6b5e4d1c1
Stephen Touset <stephen@touset.org>
2010-10-29 10:12:28 -0400

    Basic support for all input types

--- lib/semantic_form_for/form_builder.rb
+++ lib/semantic_form_for/form_builder.rb
@@ -1,10 +1,75 @@
 require 'semantic_form_for'
 
 class SemanticFormFor::FormBuilder < ActionView::Helpers::FormBuilder
-  def inputs(label = nil, &block)
-    template.content_tag :fieldset do
-      template.content_tag :legend, label if label
-      template.capture(&block)
+  attr_reader :template
+  attr_reader :object
+  attr_reader :object_name
+  
+  FIELDSETS = [ :inputs, :buttons ]
+  INPUTS    = [ :check_box,    :email_field,    :file_field,   :hidden_field,
+                :number_field, :password_field, :radio_button, :range_field,
+                :search_field, :select,         :text_area,    :text_field,
+                :url_field ]
+  BUTTONS   = [ :image_submit, :submit ]
+  
+  FIELDSETS.each do |set|
+    define_method set do |text, &block|
+      template.capture_haml do
+        template.haml_tag :fieldset, :class => set do
+          template.haml_tag(:legend, text) if text
+          template.haml_tag(:ol, &block)
+        end
+      end
     end
+  end
+  
+  INPUTS.each do |input|
+    define_method input do |attribute, *args|
+      options  = args.extract_options!
+      text     = args.shift
+      
+      template.capture_haml do
+        template.haml_tag(:li, :id => _li_id(attribute)) do
+          template.haml_concat self.label(attribute, text)
+          template.haml_concat super(attribute, options)
+        end
+      end
+    end
+  end
+  
+  BUTTONS.each do |button|
+    define_method button do |text, options = {}|
+      template.capture_haml do
+        template.haml_tag(:li, :id => nil) do
+          template.haml_concat super(text, options)
+        end
+      end
+    end
+  end
+  
+  private
+  
+  def _object_name
+    self.object_name.to_s.gsub(/\]\[|[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
+  end
+  
+  def _object_index
+    case
+      when options.has_key?(:index) then options[:index]
+      when defined?(@auto_index)    then @auto_index
+      else                               nil
+    end.to_s
+  end
+  
+  def _attribute_name(attribute)
+    attribute.to_s.gsub(/[\?\/\-]$/, '')
+  end
+  
+  def _li_id(attribute)
+    [
+      _object_name + _object_index, 
+      _attribute_name(attribute),
+      'input'
+     ].join('_')
   end
 end
