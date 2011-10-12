46fd4a1ac6cb90482cd69d53d069786d19423455
Stephen Touset <stephen@touset.org>
2011-10-12 19:34:29 -0400

    Autoload the VERSION, too. Might as well.

--- lib/twitter_bootstrap_form_for.rb
+++ lib/twitter_bootstrap_form_for.rb
@@ -1,9 +1,8 @@
 module TwitterBootstrapFormFor
-  require 'twitter_bootstrap_form_for/version'
-  
   autoload :FormBuilder, 'twitter_bootstrap_form_for/form_builder'
   autoload :FormHelpers, 'twitter_bootstrap_form_for/form_helpers'
   autoload :Railtie,     'twitter_bootstrap_form_for/railtie'
+  autoload :VERSION,     'twitter_bootstrap_form_for/version'
 end
 
 TwitterBootstrapFormFor::Railtie # trigger loading the Railtie
