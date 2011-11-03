12c8ab4e8dd320104587c7b0c1143e88a94bdc04
Pavel Pravosud <rwz@duckroll.ru>
2011-11-03 16:31:09 +0700

    only add "for" attribute to checkboxes

--- lib/twitter_bootstrap_form_for/form_builder.rb
+++ lib/twitter_bootstrap_form_for/form_builder.rb
@@ -92,11 +92,12 @@ class TwitterBootstrapFormFor::FormBuilder < ActionView::Helpers::FormBuilder
 
   TOGGLES.each do |toggle|
     define_method toggle do |attribute, *args, &block|
-      label   = args.first.nil? ? '' : args.shift
-      target  = self.object_name.to_s + '_' + attribute.to_s
+      label       = args.first.nil? ? '' : args.shift
+      target      = self.object_name.to_s + '_' + attribute.to_s
+      label_attrs = toggle == :check_box ? { :for => target } : {}
 
       template.content_tag(:li) do
-        template.concat template.content_tag(:label, :for => target) {
+        template.concat template.content_tag(:label, label_attrs) {
           template.concat super(attribute, *args)
           template.concat ' ' # give the input and span some room
           template.concat template.content_tag(:span, label)
