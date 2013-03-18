119e4723f317be379792c4c073978abad1b51805
Garami Gábor <hron@hron.me>
2013-03-18 13:07:43 +0100

    Fixing typo

--- lib/twitter_bootstrap_form_for/form_builder.rb
+++ lib/twitter_bootstrap_form_for/form_builder.rb
@@ -94,10 +94,10 @@ class TwitterBootstrapFormFor::FormBuilder < ActionView::Helpers::FormBuilder
       self.div_wrapper(attribute) do
         template.concat self.label(attribute, label) if label
         template.concat template.content_tag(:div, :class => classes.join(' ')) {
-          block.call if block.present? and classess.include?('input-prepend')
+          block.call if block.present? and classes.include?('input-prepend')
           template.concat super(attribute, *(args << options))
           template.concat error_span(attribute)
-          block.call if block.present? and classess.include?('input-append')
+          block.call if block.present? and classes.include?('input-append')
         }
       end
     end
